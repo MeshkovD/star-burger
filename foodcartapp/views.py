31fabdde46b9b0d276c63a39a91d89e262d19149
MeshkovDI_PC <rdm.dima@mail.ru>
2022-04-06 13:22:21 +0700

    Add models for order data

---
+++ foodcartapp/views.py
@@ -0,0 +1,82 @@
+import json
+
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.templatetags.static import static
+
+
+from .models import Product
+from .models import Order
+from .models import OrderItem
+
+
+def banners_list_api(request):
+    # FIXME move data to db?
+    return JsonResponse([
+        {
+            'title': 'Burger',
+            'src': static('burger.jpg'),
+            'text': 'Tasty Burger at your door step',
+        },
+        {
+            'title': 'Spices',
+            'src': static('food.jpg'),
+            'text': 'All Cuisines',
+        },
+        {
+            'title': 'New York',
+            'src': static('tasty.jpg'),
+            'text': 'Food is incomplete without a tasty dessert',
+        }
+    ], safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def product_list_api(request):
+    products = Product.objects.select_related('category').available()
+
+    dumped_products = []
+    for product in products:
+        dumped_product = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'special_status': product.special_status,
+            'description': product.description,
+            'category': {
+                'id': product.category.id,
+                'name': product.category.name,
+            } if product.category else None,
+            'image': product.image.url,
+            'restaurant': {
+                'id': product.id,
+                'name': product.name,
+            }
+        }
+        dumped_products.append(dumped_product)
+    return JsonResponse(dumped_products, safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def register_order(request):
+    try:
+        order_raw = json.loads(request.body.decode())
+    except ValueError:
+        return JsonResponse({
+                'error': 'Ошибка при чтении JSON',
+        })
+    order = Order.objects.create(
+        firstname=order_raw['firstname'],
+        lastname=order_raw['lastname'],
+        phonenumber=order_raw['phonenumber'],
+        address=order_raw['address'],
+    )
+    for product in order_raw['products']:
+        order.add_product(product['product'], product['quantity'])
+    return JsonResponse({})
+
+
