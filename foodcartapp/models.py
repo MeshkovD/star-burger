a64fefae473192abf150fb916a31515b7e3e73e8
MeshkovDI_PC <rdm.dima@mail.ru>
2022-06-01 13:46:48 +0700

    Move fetch_coordinates function to Place app

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,6 +1,5 @@
 import datetime
 
-import requests
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.db.models import F, Sum
@@ -10,6 +9,7 @@ from geopy import distance
 from phonenumber_field.modelfields import PhoneNumberField
 
 from place.models import Place
+from place.models import fetch_coordinates
 from star_burger import settings
 
 
@@ -131,22 +131,22 @@ class RestaurantMenuItem(models.Model):
         return f"{self.restaurant.name} - {self.product.name}"
 
 
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
+# def fetch_coordinates(apikey, address):
+#     base_url = "https://geocode-maps.yandex.ru/1.x"
+#     response = requests.get(base_url, params={
+#         "geocode": address,
+#         "apikey": apikey,
+#         "format": "json",
+#     })
+#     response.raise_for_status()
+#     found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+#
+#     if not found_places:
+#         return None
+#
+#     most_relevant = found_places[0]
+#     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+#     return lon, lat
 
 
 class ExtendedQuerySet(models.QuerySet):
--- place/models.py
+++ place/models.py
@@ -1,3 +1,4 @@
+import requests
 from django.db import models
 
 
@@ -36,3 +37,21 @@ class Place(models.Model):
         ordering = ['address']
         verbose_name = "Место"
         verbose_name_plural = "Места"
+
+
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
